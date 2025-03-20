89c1a13c7aaab44377110075b7808e6a4be100c3
ayush-git890 <ayushofficial4208@gmail.com>
2025-03-20 13:08:06 +0530

    [ayush]:7A-Show_Day_With_Full_Time

---
+++ UC-7C_ShowDayWithFullTimeWage.js
@@ -0,0 +1,70 @@
+const isPartTime = 1;
+const isFullTime = 2;
+const partTimeHours = 4;
+const fullTimeHours = 8;
+const wagePerHour = 20;
+const maxWorkingDays = 20;
+const maxWorkingHours = 160;
+
+function getWorkingHours(flag) {
+    switch (flag) {
+        case isPartTime:
+            return partTimeHours;
+        case isFullTime:
+            return fullTimeHours;
+        default:
+            return 0;
+    }
+}
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = [];
+
+while (totalEmpHrs < maxWorkingHours && totalWorkingDays < maxWorkingDays) {
+    totalWorkingDays++;
+    let flag = Math.floor(Math.random() * 10) % 3;
+    let dailyHours = getWorkingHours(flag);
+
+    if (totalEmpHrs + dailyHours > maxWorkingHours) {
+        dailyHours = maxWorkingHours - totalEmpHrs;
+    }
+
+    totalEmpHrs += dailyHours;
+    let dailyWage = dailyHours * wagePerHour;
+    empDailyWageArr.push(dailyWage);
+
+    console.log(`Day ${totalWorkingDays}: Work Hours = ${dailyHours}, Employee Wage = ${dailyWage}`);
+}
+
+let totEmpWage = 0;
+function sum(dailyWage) {
+    totEmpWage += dailyWage;
+}
+empDailyWageArr.forEach(sum);
+console.log("UC7A - Total Days:", totalWorkingDays, 
+    "Total Hrs:", totalEmpHrs, "Emp Wage:", totEmpWage);
+
+function totalWages(totalWage, dailyWage) {
+    return totalWage + dailyWage;
+}
+console.log("UC7A Emp Wage with reduce:", empDailyWageArr.reduce(totalWages, 0));
+
+let dailyCntr = 0;
+function mapDayWithWage(dailyWage) {
+    dailyCntr++;
+    return `Day ${dailyCntr} = ${dailyWage}`;
+}
+let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+
+console.log("UC7B - Daily Wage Map:");
+console.log(mapDayWithWageArr);
+
+// **UC 7C - Show Days when Full-time Wage of 160 was earned**
+function fulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+let fullDayWageArr = mapDayWithWageArr.filter(fulltimeWage);
+
+console.log("UC7C - Daily Wage Filter When Full-time Wage Earned:");
+console.log(fullDayWageArr);
